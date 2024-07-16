e4f52e88df2a948bc97fc6880d6e15ae35cd6360
Aditi K <61196135+SavageSanta11@users.noreply.github.com>
2024-07-16 12:37:50 +0800

    add data cleaning steps

---
+++ steps/clean_data.py
@@ -0,0 +1,21 @@
+import logging
+import pandas as pd
+from zenml import step
+
+from src.data_cleaning import DataCleaning, DataDivideStrategy, DataPreprocessStrategy
+
+@step
+def clean_df(df: pd.DataFrame):
+    try:
+        process_strategy = DataPreprocessStrategy()
+        data_cleaning = DataCleaning(df, process_strategy)
+        processed_data = data_cleaning.handle_data()
+
+        divide_strategy = DataDivideStrategy()
+        data_cleaning = DataCleaning(processed_data, divide_strategy)
+        X_train, X_test, y_train, y_test = data_cleaning.handle_data()
+        logging.info("Data cleaning completed")
+
+    except Exception as e:
+        logging.error("Error in cleaning data: {}".format(e))
+        raise e
